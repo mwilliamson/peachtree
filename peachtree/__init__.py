781ba8b62936e7dea41029b21d7c059227ff575c
Michael Williamson <mike@zwobble.org>
2013-01-03 21:41:45 +0000

    Test kvm port forwarding

--- peachtree/__init__.py
+++ peachtree/__init__.py
@@ -1,7 +1,8 @@
 from . import qemu
 
-def start_kvm(machine_name):
+
+def start_kvm(machine_name, public_ports=None):
     provider = qemu.QemuProvider()
-    vm = provider.create(machine_name, [])
+    vm = provider.create(machine_name, public_ports or [])
     vm.start()
     return vm
--- tests/qemu_tests.py
+++ tests/qemu_tests.py
@@ -2,9 +2,24 @@ from nose.tools import istest, assert_equals
 
 import peachtree
 
+
 @istest
 def can_run_commands_on_vm():
     with peachtree.start_kvm("ubuntu-precise-amd64") as vm:
         shell = vm.shell()
+        result = shell.run(["echo", "Hello there"])
+        assert_equals("Hello there\n", result.output)
+
+
+@istest
+def can_ensure_that_ports_are_available():
+    with peachtree.start_kvm("ubuntu-precise-amd64", public_ports=[50022]) as vm:
+        root_shell = vm.root_shell()
+        root_shell.run(["sh", "-c", "echo Port 50022 >> /etc/ssh/sshd_config"])
+        root_shell.run(["service", "ssh", "restart"])
+        
+        ssh_config = vm.ssh_config()
+        ssh_config.port = vm.public_port(50022)
+        shell = ssh_config.shell()
         result = shell.run(["echo", "Hello there"])
         assert_equals("Hello there\n", result.output)
