d6b860e6b5e0501679c9115da6076f64541f714b
Michael Williamson <mike@zwobble.org>
2013-01-06 23:08:42 +0000

    Factor out duplicated code in remote_tests

--- tests/remote_tests.py
+++ tests/remote_tests.py
@@ -19,24 +19,28 @@ logging.getLogger("paramiko").setLevel(logging.WARNING)
 _IMAGE_NAME="ubuntu-precise-amd64"
 
 
-provider = peachtree.qemu_provider()
+underlying_provider = peachtree.qemu_provider()
 
 @istest
 def can_run_commands_on_vm():
-    port = starboard.find_local_free_tcp_port()
-    with peachtree.server.start_server(port=port, provider=provider):
-        with peachtree.remote_provider(hostname="localhost", port=port) as client:
-            with client.start(_IMAGE_NAME) as machine:
-                shell = machine.shell()
-                result = shell.run(["echo", "Hello there"])
-                assert_equals("Hello there\n", result.output)
+    with _create_remote_provider() as provider:
+        with provider.start(_IMAGE_NAME) as machine:
+            shell = machine.shell()
+            result = shell.run(["echo", "Hello there"])
+            assert_equals("Hello there\n", result.output)
 
 
 @istest
 def machine_is_not_running_after_context_manager_for_machine_exits():
+    with _create_remote_provider() as provider:
+        with provider.start(_IMAGE_NAME) as machine:
+            assert machine.is_running()
+        assert not machine.is_running()
+
+
+@contextlib.contextmanager
+def _create_remote_provider():
     port = starboard.find_local_free_tcp_port()
-    with peachtree.server.start_server(port=port, provider=provider):
-        with peachtree.remote_provider(hostname="localhost", port=port) as client:
-            with client.start(_IMAGE_NAME) as machine:
-                assert machine.is_running()
-            assert not machine.is_running()
+    with peachtree.server.start_server(port=port, provider=underlying_provider):
+        with peachtree.remote_provider(hostname="localhost", port=port) as provider:
+            yield provider
