8b7ccde41a51595ce268d3a9c467e24b58cab36b
Michael Williamson <mike@zwobble.org>
2013-01-12 21:57:26 +0000

    Allow starting of machines from web interface

--- web/main.js
+++ web/main.js
@@ -8,4 +8,20 @@ angular.module('peachtree', [])
             .error(function(data, status, headers, config) {
             });
         $scope.machines = []
+    })
+    
+    .controller("AvailableImagesController", function($scope, $http) {
+        $http({method: 'POST', url: '/available-images'})
+            .success(function(data, status, headers, config) {
+                var images = data;
+                images.sort();
+                $scope.images = images;
+            })
+            .error(function(data, status, headers, config) {
+            });
+        
+        $scope.start = function(imageName) {
+            var arguments = {"image-name": imageName, "public-ports": "22"};
+            $http({method: "POST", url: "/start", data: arguments});
+        };
     });
