ae62c79c3c055019bbe8d39d7f9d1c6ecbb5e35b
Michael Williamson <mike@zwobble.org>
2013-02-05 22:49:43 +0000

    Use OrderedDict to order elements in machine description

--- peachtree/machine_description.py
+++ peachtree/machine_description.py
@@ -1,11 +1,13 @@
+import collections
+
 from . import dictobj
 
 
 def describe_machine(machine):
-    return {
-        "identifier": machine.identifier,
-        "name": machine.name,
-        "imageName": machine.image_name,
-        "externalHostname": machine.external_hostname(),
-        "users": map(dictobj.obj_to_dict, machine.users()),
-    }
+    return collections.OrderedDict([
+        ("identifier", machine.identifier),
+        ("name", machine.name),
+        ("imageName", machine.image_name),
+        ("externalHostname", machine.external_hostname()),
+        ("users", map(dictobj.obj_to_dict, machine.users())),
+    ])
