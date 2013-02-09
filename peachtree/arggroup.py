a3105fa18f4c9d9dd1f98379bc0a839325cb3e01
Michael Williamson <mike@zwobble.org>
2013-02-09 12:44:53 +0000

    Add argument parsing for repeatable argument groups

---
+++ peachtree/arggroup.py
@@ -0,0 +1,40 @@
+import argparse
+
+
+def add_repeatable_argument_group(parser, name):
+    group_parser = argparse.ArgumentParser()
+    
+    class RepeatableGroupAction(argparse.Action):
+        def __init__(self, *args, **kwargs):
+            argparse.Action.__init__(
+                self,
+                *args,
+                required=False,
+                nargs=argparse.REMAINDER,
+                default=[],
+                **kwargs
+            )
+        
+        def __call__(self, parser, namespace, values, option_string=None):
+            if not hasattr(namespace, self.dest):
+                setattr(namespace, self.dest, [])
+            
+            remaining_argv = values
+            
+            while remaining_argv:
+                if name in remaining_argv:
+                    group_end_index = remaining_argv.index(name)
+                    group_argv = remaining_argv[:group_end_index]
+                else:
+                    group_end_index = len(remaining_argv)
+                    group_argv = remaining_argv
+                    
+                result = group_parser.parse_args(group_argv)
+                
+                getattr(namespace, self.dest).append(result)
+                
+                remaining_argv = remaining_argv[group_end_index + 1:]
+            
+    
+    parser.add_argument("--requests", action=RepeatableGroupAction)
+    return group_parser
---
+++ tests/arggroup_tests.py
@@ -0,0 +1,51 @@
+import argparse
+
+from nose.tools import istest, assert_equal
+
+from peachtree import arggroup
+
+
+@istest
+def empty_list_of_arguments_is_parsed_as_empty_list():
+    argv = []
+    
+    parser = argparse.ArgumentParser()
+    arggroup.add_repeatable_argument_group(parser, "--request")
+    
+    args = parser.parse_args(argv)
+    assert_equal([], args.requests)
+
+
+@istest
+def repeatable_argument_group_uses_subparser_to_parse_args():
+    argv = ["--request", "--name", "server", "--image-name", "debian"]
+    
+    parser = argparse.ArgumentParser()
+    group_parser = arggroup.add_repeatable_argument_group(parser, "--request")
+    group_parser.add_argument("--name")
+    group_parser.add_argument("--image-name")
+    
+    args = parser.parse_args(argv)
+    assert_equal(1, len(args.requests))
+    assert_equal("server", args.requests[0].name)
+    assert_equal("debian", args.requests[0].image_name)
+
+
+@istest
+def group_name_is_used_to_indicate_next_element():
+    argv = [
+        "--request", "--name", "server", "--image-name", "debian",
+        "--request", "--name", "client", "--image-name", "ubuntu",
+    ]
+    
+    parser = argparse.ArgumentParser()
+    group_parser = arggroup.add_repeatable_argument_group(parser, "--request")
+    group_parser.add_argument("--name")
+    group_parser.add_argument("--image-name")
+    
+    args = parser.parse_args(argv)
+    assert_equal(2, len(args.requests))
+    assert_equal("server", args.requests[0].name)
+    assert_equal("debian", args.requests[0].image_name)
+    assert_equal("client", args.requests[1].name)
+    assert_equal("ubuntu", args.requests[1].image_name)
