b1e38752b7ed2bae19c25f558c169c650a696f84
Michael Williamson <mike@zwobble.org>
2013-02-20 09:36:18 +0000

    Bump version to 0.4.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='peachtree',
-    version='0.4.3',
+    version='0.4.4',
     description='Library for starting and interacting with qemu/kvm',
     long_description=read("README"),
     author='Michael Williamson',
